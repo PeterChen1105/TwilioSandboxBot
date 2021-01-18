3027896a5bd983eaaec24560495ac71923bfb1a8
Peter.Chen1 <peter.chen@monash.edu>
2021-01-18 11:00:17 +1100

    Reply with image

--- bot.js
+++ bot.js
@@ -1,31 +1,51 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const {
+  ActivityHandler,
+  MessageFactory,
+  ActivityTypes,
+} = require("botbuilder");
 
 class EchoBot extends ActivityHandler {
-    constructor() {
-        super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
+  constructor() {
+    super();
+    // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+    this.onMessage(async (context, next) => {
+      const msg = context.activity.text;
+      if (msg === "1") {
+        const replyText = "Test Text Message";
+        await context.sendActivity(MessageFactory.text(replyText, replyText));
+      } else if (msg === "2") {
+        const reply = { type: ActivityTypes.Message };
+        reply.attachments = [
+          {
+            contentType: "image/jpeg",
+            contentUrl:
+              "https://www.flaticon.com/svg/vstatic/svg/1766/1766528.svg?token=exp=1610928854~hmac=a370888ab39a1f2a2479e3e28c086f2a",
+          },
+        ];
+        await context.sendActivity(reply);
+      }
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-                }
-            }
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
-    }
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      const welcomeText = "Hello and welcome!";
+      for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+        if (membersAdded[cnt].id !== context.activity.recipient.id) {
+          await context.sendActivity(
+            MessageFactory.text(welcomeText, welcomeText)
+          );
+        }
+      }
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+  }
 }
 
 module.exports.EchoBot = EchoBot;
